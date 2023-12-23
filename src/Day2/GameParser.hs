5217067de6cb48fa35382d3c975e21acc0ddfd08
James Packard <40209595+packard-j@users.noreply.github.com>
2023-12-22 23:22:43 -0500

    document game parser

--- src/Day2/GameParser.hs
+++ src/Day2/GameParser.hs
@@ -7,17 +7,23 @@ import Data.Attoparsec.ByteString (Parser, sepBy, string)
 import Data.Attoparsec.ByteString.Char8 (decimal, space)
 import Day2.Game
 
+-- | Parses a game in the form:
+-- | Game <id>: <color> <int>, <color> <int>; <color <int> ...
 gameParser :: Parser Game
 gameParser = do
   gid <- string "Game " >> decimal <* ": "
   selections <- selectionParser `sepBy` string "; "
   return $ Game gid selections
 
+-- | Parses a cube selection in the form:
+-- | <color> <int>, <color> <int> ...
 selectionParser :: Parser CubeSelection
 selectionParser = CubeSelection <$> colorCount `sepBy` string ", "
   where
     colorCount = flip (,) <$> (decimal <* space) <*> colorParser
 
+-- | Parses a cube color, which is one of:
+-- | "red" | "green" | "blue"
 colorParser :: Parser Color
 colorParser =
   (string "red" >> pure Red)
